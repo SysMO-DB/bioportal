305a319ac44d7fe683f0328f5282107b97986ce3
quyen <nttqa22001@yahoo.com>
2013-05-21 15:37:19 +0200

    fix including BioPortalHelper module

--- rails/init.rb
+++ rails/init.rb
@@ -1,4 +1,4 @@
 require 'bioportal'
 
-ActionView::Base.send(:include, BioPortal::BioPortalHelper)
+ActionView::Base.send(:include, BioPortalHelper)
 ActiveRecord::Base.send(:include,BioPortal::Acts)
